02a442c471d637870e0bd1760cf0b8f48bc449a5
David <davidbalyan9@gmail.com>
2021-11-11 23:29:32 +0200

    Added some links

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import Footer from "./Components/Footer/Footer";
 import Gallery from "./Components/Gallery/Gallery";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
 	return (
-		<div>
-			<Header />
-			<Hero />
-			<About />
-			<Gallery />
-			<Footer />
-		</div>
+		<Router>
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path='/'>
+						<Hero />
+						<About id='about' />
+					</Route>
+					<Route path='/gallery'>
+						<Gallery />
+					</Route>
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
 	);
 };
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "./gem-logo.png";
 import { FiArrowDown } from "react-icons/fi";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	const [show, setShow] = useState(false);
@@ -15,9 +16,15 @@ const Header = () => {
 					<FiArrowDown className='menu-arrow' />
 				</div>
 				<nav id={show ? "" : "hide"}>
-					<p>Home</p>
-					<p>Gallery</p>
-					<p>About</p>
+					<Link to='/' className='p'>
+						Home
+					</Link>
+					<Link to='/gallery' className='p'>
+						Gallery
+					</Link>
+					<Link to='/#about' className='p'>
+						About
+					</Link>
 				</nav>
 			</div>
 		</div>
